169bf1b9776529cb1e2e8335c159dfd652bfa509
ppudodo <dkatavic03@gmail.com>
2024-03-24 08:40:29 +0100

    Testing mongo db connectivity

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import userRoute from "./routes/users.js"
 import dotenv from "dotenv";
 const app = express();
 dotenv.config();
+let test;
 const connectToDataBase = async ()=>{
    
     try {
@@ -17,16 +18,18 @@ const connectToDataBase = async ()=>{
 }
 mongoose.connection.on("disconnected",()=>{
     console.log("MongoDB disconnected")
+    test = 0;
 });
 mongoose.connection.on("connected",()=>{
     console.log("MongoDB connected")
+    test=1;
 });
 
 app.use(express.json())
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the API"); // You can customize this response as needed
+    res.send("Welcome to the API",test); // You can customize this response as needed
   });
 
 app.use("/api/auth",authRoute);
